ff11e9156b0162cd3018f1e3e82b973e54a5dbd9
kousalyadhulipalla <82784311+kousalyadhulipalla@users.noreply.github.com>
2021-04-19 09:34:44 -0400

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import mongoose from 'mongoose';
+import { typeDefs } from "./models/typedefs";
+const schema = require('./models/typedefs');
+import {resolvers} from "./models/resolver";
+import bodyParser from "body-parser";
+import { graphqlExpress } from "graphql-server-express";
+
+ const startServer= async ()=>{
+  const app = express();
+  const server = new ApolloServer({typeDefs,resolvers});
+  server.applyMiddleware({app});
+  await mongoose.connect('mongodb://localhost:27017/',{useNewUrlParser:true, useUnifiedTopology:true})
+  .catch(err => console.log(err))
+  // app.use('/graphql', bodyParser.json(), graphqlExpress({ schema:schema  }));
+  app.listen({port:4000}, () =>
+  console.log(`🚀  Server ready `) 
+  );
+ }
+
+startServer();
+
